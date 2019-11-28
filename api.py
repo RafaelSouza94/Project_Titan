fb8372f38517e20e4f96d7b3681b447c0e171833
Rafael Souza <rafaelrochasouza@outlook.com.br>
2019-11-27 19:23:23 -0500

    Trying to make sphinx work

--- api.py
+++ api.py
@@ -1,9 +1,31 @@
+"""
+Basic API module
+
+.. moduleauthor:: Rafael Souza <https://github.com/RafaelSouza94>
+
+"""
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET', 'POST'])
-def test():
+def api_main():
+    """
+    **Main API view**
+          
+    Main API module. 
+    
+    :return: For now, just a Status: Working JSON
+    
+    - Example::
+        Nothing really, just testing still
+     
+    - Expected Success Response::
+        HTTP Status Code: 200
+        {'Status': 'Working'}
+        
+    """
     return jsonify({'Status': 'Working'})
 
 @app.route('/tretas', methods=['GET'])
