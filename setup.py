9f5274b1c1b459fe3dd725a137679c6a46e35c09
Rafael Souza <rafaelrochasouza@outlook.com.br>
2019-12-04 09:27:10 -0500

    fixing install

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open(README.md) as f:
+with open("./README.md") as f:
     LONG_DESC = f.read()
     
 setup(name='Titan',
@@ -14,8 +14,8 @@ setup(name='Titan',
       classifiers=[
           'Development Status :: 3 - Alpha', 
           'License :: OSI Approved :: GNU GPLv3',
-          'Programming Language :: Python :: 3']
-      keywords=['flask', 'security', 'threat intelligence']
+          'Programming Language :: Python :: 3'],
+      keywords=['flask', 'security', 'threat intelligence'],
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
