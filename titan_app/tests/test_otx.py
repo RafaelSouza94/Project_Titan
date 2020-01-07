1ddc73fe18f8390da357edc6304e17fe2a16c87b
Rafael Souza <rafaelrochasouza@outlook.com.br>
2020-01-07 09:28:49 -0500

    Added tests to getinfo function

--- titan_app/tests/test_otx.py
+++ titan_app/tests/test_otx.py
@@ -17,28 +17,28 @@ class TestOTX(unittest.TestCase):
         
     def test_otx_ip_info_not_json(self):
         otx_ip_info = self.tested_app.post(
-            '/otx/getinfoip',
+            '/otx/getinfo/ip',
             {"ip":"113.52.135.33"}
         )
         assert "Error" in otx_ip_info
         
     def test_otx_ip_info_no_ip(self):
-        otx_ip_info = self.tested_app.post(
-            '/otx/getinfoip',
+        otx_ip_info = self.tested_app.post_json(
+            '/otx/getinfo/ip',
             {"nothing":"again"}
         )
         assert "Error" in otx_ip_info
         
     def test_otx_url_info_not_json(self):
         otx_url_info = self.tested_app.post(
-            '/otx/getinfourl',
-            {"url":"http://google.com"}
+            '/otx/getinfo/url',
+            {"url":"google.com"}
         )
         assert "Error" in otx_url_info
         
     def test_otx_url_info_no_url(self):
-        otx_url_info = self.tested_app.post(
-            '/otx/getinfourl',
+        otx_url_info = self.tested_app.post_json(
+            '/otx/getinfo/url',
             {"nothing":"again"}
         )
         assert "Error" in otx_url_info    
